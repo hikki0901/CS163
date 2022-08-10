5a4feb5f2ab77fff74bbaf1dbac19a1d4c0d67fc
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-08-11 00:33:01 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include "UI.h"
 
 int main() {
-  HashTable* dict = new HashTable[26];
+	HashTable* dict = new HashTable[26];
 	for (int i = 0; i < 26; i++) {
-    dict[i].head = NULL;
+    		dict[i].head = NULL;
 		dict[i].tail = NULL;
 	}
-  return 0;
+	return 0;
 }
