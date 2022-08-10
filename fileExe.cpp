2ecc2448ccd0eb9ef68e6cbd8352bc3f24448d4c
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-08-10 23:56:50 +0700

    Update fileExe.cpp

--- fileExe.cpp
+++ fileExe.cpp
@@ -1,48 +1,63 @@
 #include "fileExe.h"
-#include "mainStruct.h"
 #include <string>
 
 using namespace std;
 
-WordInfo transfer(string r) {
-    WordInfo tmp;
-    int i = 0;
-    int pos;
+void split(string s, vector<string>& defs){
+    vector<int> pos;
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] == '-') pos.push_back(i);
 
-    string Word = "";
-    while (r[i] != '/') {
-        Word += r[i++];
+    pos.push_back(s.size() + 1);
+    for (int i = 0; i < pos.size() - 1; i++) {
+        string token = s.substr(pos[i] + 1, pos[i+1] - 2);
+        defs.push_back(token);
     }
-    tmp.word = Word; i++;
-    
-    string Type = "";
-    while (r[i] != '/') {
-        Type += r[i++];
+
+}
+
+WordInfo transfer(string r) {
+
+    WordInfo tmp;
+    int i = 0; string ID = "";
+
+    while ((int(r[i]) >= 48) && (int(r[i]) <= 57)) ID += r[i++];
+    tmp.ID = stoi(ID, 0, 10); r.erase(0, i + 1);
+
+    i = 0; string w = "";
+    while (r[i] != ' ') w += r[i++];
+    tmp.word = w;
+
+    if (r[0] == '-') {
+        r.erase(0, i + 2);
+        tmp.def.push_back(r);
+        return tmp;
+    } else {
+        i = 0; r.erase(0, i + 2);
+
+        if (r[i] != '-') {
+            string t = "";
+            while (r[i] != '.') t += r[i++];
+
+            tmp.type.push_back(t);
+            r.erase(0, i + 2);
+            tmp.def.push_back(r);
+        } else {
+            vector<string> defs;
+            split(r, defs);
+
+            for (int i = 0; i < defs.size(); i++) {
+                int j = 0; string t = "";
+                while (defs[i][j] != '.') t += defs[i][j++];
+
+                tmp.type.push_back(t);
+                defs[i].erase(0, j + 2);
+                tmp.def.push_back(defs[i]);
+            }
+
+        }
     }
-    tmp.type = Type; i++;
-    
-    tmp.curWordsCount = int(r[i]) - 48;
-    i += 2;
-    
-    while (r[i] != '/') {
-        string Def = "";
-        while (r[i] != ';') {
-            Def += r[i++];
-        }
-        tmp.def.push_back(Def);
-        i++;
-    } 
-    i++;
-    
-    while (r[i] != '/') {
-        string Examples = "";
-        while (r[i] != ';') {
-            Examples += r[i++];
-        }
-        tmp.examples.push_back(Examples);
-        i++;
-    } 
-	return tmp;
+    return tmp;
 }
 
 void fileRead(hashing*& dict) {
