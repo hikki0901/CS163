66375d047ecfd950c08146bf29b5d09a65974f9e
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-08-11 00:31:14 +0700

    Update fileExe.h

--- fileExe.h
+++ fileExe.h
@@ -3,10 +3,10 @@
 #include <string>
 #include <cstdlib>
 
-void fileRead(HashTable* dict);
-void fileWrite(HashTable* dict);
-void fileReset(HashTable* dict);
+void fileRead(HashTable*& dict);
+void fileWrite(HashTable*& dict);
+void fileReset(HashTable*& dict);
 
-void removeWord(HashTable* dict, WordInfo& word);
+void removeWord(HashTable*& dict, WordInfo& word);
 void split(string s, vector<string>& defs);
 WordInfo transfer(string r);
