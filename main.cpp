34e096f47f23ce065400a2fb95783c3be95260a8
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-08-10 23:01:41 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 #include "UI.h"
 
 int main() {
+  Wordlist* hashTable = new Wordlist[26];
+  for (int i = 0; i < 26; i++) {
+        hashTable[i]->left = NULL;
+        hashTable[i]->right = NULL;
+  }
   return 0;
 }
