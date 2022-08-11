ac146834f9f99456226b6fc2002689803b5d4e83
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-08-11 18:17:43 +0700

    Update fileExe.h

--- fileExe.h
+++ fileExe.h
@@ -7,6 +7,7 @@ void fileRead(HashTable*& dict);
 void fileWrite(HashTable*& dict);
 void fileReset(HashTable*& dict);
 
-void removeWord(HashTable*& dict, WordInfo& word);
+void removeWord(HashTable*& dict, Wordlist*& word);
+void InsertTable(HashTable*& dict, Wordlist* new_word);
 void split(string s, vector<string>& defs);
 WordInfo transfer(string r);
